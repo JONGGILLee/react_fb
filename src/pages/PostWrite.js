21ebd46da3f3681aa5484d8b69806b2602115b29
JONGGIL LEE <brianlee2k@naver.com>
2022-04-07 09:58:34 +0900

    3-4강 포스트 작성하기(1) / 로그인 후에만 /write 페이지에 접근할 수 있도록 하기 / 유저가 게시글을 작성할때 콘솔에 찍히도록 const [contents, setContents] = React.useState("")

--- src/pages/PostWrite.js
+++ src/pages/PostWrite.js
@@ -3,7 +3,37 @@ import React from "react";
 import { Grid, Button, Image, Input, Text } from "../elements";
 import Upload from "../shared/Upload";
 
+import { useSelector, useDispatch } from "react-redux";
+
 const PostWrite = (props) => {
+  const is_login = useSelector((state) => state.user.is_login);
+  const { history } = props;
+
+  const [contents, setContents] = React.useState("");
+  const changeContents = (e) => {
+    setContents(e.target.value);
+  };
+  // changeContents 는 event 를 받아와서 setContents 해준다.
+  console.log(contents);
+
+  if (!is_login) {
+    return (
+      <Grid margin="90px 0px" padding="15px" center>
+        <Text size="32px" bold>
+          !!!
+        </Text>
+        <Text size="22px">로그인을 해야 게시글을 작성할 수 있습니다!</Text>
+        <Button
+          _onClick={() => {
+            history.replace("/");
+          }}
+        >
+          로그인 페이지로 이동
+        </Button>
+      </Grid>
+    );
+  }
+
   return (
     <React.Fragment>
       <Grid padding="14px">
@@ -23,7 +53,12 @@ const PostWrite = (props) => {
       </Grid>
 
       <Grid padding="14px">
-        <Input label="게시글 내용" placeholder="게시글 작성..." multiLine />
+        <Input
+          _onChange={changeContents}
+          label="게시글 내용"
+          placeholder="게시글 작성..."
+          multiLine
+        />
       </Grid>
 
       <Grid padding="14px">
